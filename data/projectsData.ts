9a37a7da9ca2b89d82b716423289d21465e72f88
adgsenpai <adg@adgstudios.co.za>
2023-04-12 23:10:10 +0200

    Added base

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,20 +1,45 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Anime @ ADGSTUDIOS',
+    description: `Watch anime shows, tv, movies for free on your mobile, tablet, or pc.
+    `,
+    imgSrc: '/static/images/animeflix.gif',
+    href: 'https://anime.adgstudios.co.za',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Manga @ ADGSTUDIOS',
+    description: `Read manga online for free on your mobile, tablet, or pc.`,
+    imgSrc: '/static/images/manga.webp',
+    href: 'https://manga.adgstudios.co.za',
   },
+  {
+    title: 'MerchGPT',
+    description: `MerchGPT is an AI website that generates designs for you to sell on Merch. 3 Credits are available on signup for free. It also allows you to design artwork from prompts.`,
+    imgSrc: '/static/images/merchgpt.png',
+    href: 'https://merchgpt.adgstudios.co.za',
+  },
+  {
+    title: 'OpportunityStonks',
+    description: `OpportunityStonks is an AI website used to predict the trend of a stock. It uses a combination of machine learning and neural networks to predict the trend of a stock. It also allows you to view the trend of a stock over a period of time.`,
+    imgSrc: '/static/images/stonksmoon.jpg',
+    href: 'https://stonks.adgstudios.co.za' 
+  },
+  {
+    title: 'Rahn Monitor',
+    description: `Your one stop compliance Solution to money laundering.
+    We offer you a platform for Anti Money Laundering and Counter Terrorism Financing services. We assist Many clients comply with their PEP/Sanction and Adverse Media screening obligations.    
+    `,
+    imgSrc: '/static/images/rm.png',
+    href: 'https://rahnmonitor.co.za',
+  },
+  {
+    title: 'ZondoScan',
+    description: `Zondo Scan. Ensure that your clients are not implicated in State Capture by the Zondo Commission. Only Vol. 1: South African Airways and its Associated ...`,
+    imgSrc: '/static/images/searchanimation.gif',
+    href: 'https://zondoscan.co.za' 
+  },
+
+
 ]
 
 export default projectsData
